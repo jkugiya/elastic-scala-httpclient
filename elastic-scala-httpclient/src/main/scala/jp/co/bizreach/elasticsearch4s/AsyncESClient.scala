ca3087c650a14d472a8a23a41ba3c78f9e9f38f3
CHAABANE Jala <CHAABANE Jala>
2016-11-25 11:42:39 +0100

    Add put mapping async

--- elastic-scala-httpclient/src/main/scala/jp/co/bizreach/elasticsearch4s/AsyncESClient.scala
+++ elastic-scala-httpclient/src/main/scala/jp/co/bizreach/elasticsearch4s/AsyncESClient.scala
@@ -200,6 +200,17 @@ class AsyncESClient(queryClient: AbstractClient, httpClient: AsyncHttpClient, ur
     insertJsonAsync(config, JsonUtils.serialize(entity))
   }
 
+  def putMappingAsync(config: ESConfig, name: String, entityType: String, entity: AnyRef): Future[Either[Map[String, Any], Map[String, Any]]] = {
+    val json = JsonUtils.serialize(entity)
+
+    logger.debug(s"put mapping $name: $json")
+    val future = HttpUtils.putAsync(httpClient, s"${config.url(url)}/$name/_mapping/$entityType", json)
+    future.map { resultJson =>
+      val map = JsonUtils.deserialize[Map[String, Any]](resultJson)
+      map.get("error").map { case message: String => Left(map) }.getOrElse(Right(map))
+    }
+  }
+
   def insertAsync(config: ESConfig, id: String, entity: AnyRef):  Future[Either[Map[String, Any], Map[String, Any]]] = {
     insertJsonAsync(config, id, JsonUtils.serialize(entity))
   }
